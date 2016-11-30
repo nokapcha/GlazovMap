5834788a4139b7e05515e53512752b13ac5104a4
AsciiShell <asciishell@gmail.com>
2016-12-01 01:23:27 +0400

    Add polygon, $ changed to spaces, refactoring
    Нет защиты от дурака
    Код какаха
    Где наш дизайнер с дизайнером?
    Кто придумает еще более тупую идею?
    Нужно добавить многострочные подсказки

--- cgi-bin/map.py
+++ cgi-bin/map.py
@@ -3,9 +3,25 @@ import cgi
 import sys
 import codecs
 import os
+
+#sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
+targetID = list(range(0,100))#get from url
+MapScript = """
+ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [58.140429, 52.674267],
+            zoom: 14,
+            controls: []
+        }, {
+            searchControlProvider: 'yandex#search'
+        })
+"""#Start script
+
+
+
 def getJSmark(Mid, x,y,text,ico):
     s = ",\n myPlacemark" + str(Mid) + " = new ymaps.Placemark([" + str(x) + ", " + str(y) + "], {\n" 
-    s += "hintContent: '" + text[:-1] + "',\n"
+    s += "hintContent: '" + text + "',\n"
     s += "balloonContent: ''\n" 
     s += "}, {\n" 
     s += "iconLayout: 'default#image',\n"
@@ -14,45 +30,111 @@ def getJSmark(Mid, x,y,text,ico):
     s += "iconImageOffset: [-16, -16]\n})"
     return s
 
-sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
-
-
-targetID = list(range(0,100))#get from url
-obj = open("objects/objects.txt", 'r')
-objs = obj.readlines()
-obj.close()
-importFiles = []
-importPoints = []
-MapScript = """
-ymaps.ready(function () {
-    var myMap = new ymaps.Map('map', {
-            center: [58.140429, 52.674267],
-            zoom: 14
-        }, {
-            searchControlProvider: 'yandex#search'
-        })
-"""
-for line in objs:
-    if line == "\n":
-        continue
-    data = line.split()
-    if int(data[0]) in targetID:
-        #Add
-        importFiles.append((data[1],data[2]))
-
-
-####################################
-for i in importFiles:
-    obj = open("objects/" + i[0], 'r')
+#Open&Get std marks
+def getObjects():
+    global targetID
+    importFiles = []
+    #Main file of mark objects
+    obj = open("objects/objects.txt", 'r')
     objs = obj.readlines()
-    for j in objs:
-        data = j.split('$')
-        importPoints.append(int(data[0]))
-        MapScript += getJSmark(int(data[0]), float(data[1]), float(data[2]), data[3], i[1]) + "\n"
     obj.close()
-MapScript += ";"
-for i in importPoints:
-    MapScript += "myMap.geoObjects.add(myPlacemark" + str(i) + ");\n"
+    for line in objs:
+        if line == "\n":
+            continue
+        data = line.split()
+        #Check dataType in target IDs
+        if int(data[0]) in targetID:
+            #Add
+            importFiles.append((data[1],data[2]))
+    return importFiles
+
+#Add marks to script
+def getMarks():
+    global MapScript
+    importFiles = getObjects()
+    importPoints = []
+    for i in importFiles:
+        obj = open("objects/" + i[0], 'r')
+        objs = obj.readlines()
+        obj.close()
+        for j in objs:
+            data = j.split()
+            importPoints.append(int(data[0]))
+            MapScript += getJSmark(int(data[0]), float(data[1]), float(data[2]), " ".join(data[3:]), i[1]) + "\n"
+        
+    MapScript += ";"
+    for i in importPoints:
+        MapScript += "myMap.geoObjects.add(myPlacemark" + str(i) + ");\n"
+
+
+def getJSpoly(Mid, points, text, backColor, MainColor, alpha, bold):
+    s = "\n myGeoObject" + str(Mid) + """ = new ymaps.GeoObject({geometry: {type: "Polygon",coordinates: [["""
+    s += points + ',\n'
+    s += """]],fillRule: "nonZero"},properties: {hintContent:" """
+    s += text + """ "\n  }}, {fillColor:"""
+    if backColor == "std":
+        s += "'#00FF00',\n"
+    else:
+        s += "'" + backColor + "',\n"
+        
+    if MainColor == "std":
+        s += "strokeColor: '#0000FF',\n"
+    else:
+        s += "strokeColor: '" + MainColor + "',\n"
+        
+    if alpha == "std":
+        s += "opacity: 0.5,\n"
+    else:
+        s += "opacity: " + alpha + ",\n"
+        
+    if bold == "std":
+        s += "strokeWidth: 5,\n"
+    else:
+        s += "opacity:" + bold + ",\n"
+    s += "}),"
+    return s
+
+#Open&Get std poly
+def getPolyFiles():
+    global targetID
+    importFiles = []
+    #Main file of mark objects
+    obj = open("polygon/polygon.txt", 'r')
+    objs = obj.readlines()
+    obj.close()
+    for line in objs:
+        if line == "\n":
+            continue
+        data = line.split()
+        #Check dataType in target IDs
+        if int(data[0]) in targetID:
+            #Add
+            importFiles.append((data[1]))
+    return importFiles
+
+#Add marks to script
+def getPoly():
+    global MapScript
+    importFiles = getPolyFiles()
+    importPoints = []
+    for i in importFiles:
+        obj = open("polygon/" + i, 'r')
+        objs = obj.readlines()
+        obj.close()
+        for j in objs:
+            data = j.split()
+            importPoints.append(int(data[0]))
+            MapScript += getJSpoly(data[0], data[1], " ".join(data[6:]), data[2],data[3],data[4],data[5]) + "\n"
+        
+    MapScript = MapScript[:-2] + ";"
+    for i in importPoints:
+        MapScript += "myMap.geoObjects.add(myGeoObject" + str(i) + ");\n"
+    #Finalization
+    #MapScript += "});"
+    
+getMarks()
+getPoly()
+#Finalization
 MapScript += "});"
 #sys.exit(0)     
 print("Content-type: text/html\n")
@@ -61,10 +143,11 @@ print("""
 <!DOCTYPE html>
 <html>
 <head>
+<meta http-equiv="Content-Type" content="text/html; charset="utf-8" />
     <title>Карта Глазова с метками</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    
     <!-- Если вы используете API локально, то в URL ресурса необходимо указывать протокол в стандартном виде (http://...)-->
-    <script src="//api-maps.yandex.ru/2.1/?lang=ru_RU" type="text/javascript"></script>
+    <script src="//api-maps.yandex.ru/2.1/?lang=en_RU" type="text/javascript"></script>
     
     <script type="text/javascript">
 """)
@@ -76,14 +159,14 @@ print("""
 
 	<style>
         html, body, #map {
-            width: 90%; height: 100%; padding: 0; margin: 0;
+            width: 100%; height: 100%; padding: 0; margin: 0;
         }
     </style>
 </head>
 <body>
 
-<div style="width:10%; float:left"> <p>Что-то работает</p> </div>
-<div id="map" style="width:100% ;float:right"></div>
+
+<div id="map" ></div>
 </body>
 </html>
 """)
--- mark.js
+++ mark.js
@@ -9,14 +9,24 @@ ymaps.ready(function () {
             searchControlProvider: 'yandex#search'
         })
         ,
-        myPlacemark0 = new ymaps.Placemark([100.0, 100.0], {
-            hintContent: 'decsriptio',
+        myPlacemark0 = new ymaps.Placemark([58.141162, 52.643032], {
+            hintContent: 'decsription',
             balloonContent: ''
         }, {
                 iconLayout: 'default#image',
                 iconImageHref: '/images/schools.png',
                 iconImageSize: [32, 32],
-                iconImageOffset: [0, 0]
+                iconImageOffset: [-16, -16]
+            })
+        ,
+        myPlacemark100 = new ymaps.Placemark([58.172289, 52.636147], {
+            hintContent: 'School no -1',
+            balloonContent: ''
+        }, {
+                iconLayout: 'default#image',
+                iconImageHref: '/images/schools.png',
+                iconImageSize: [32, 32],
+                iconImageOffset: [-16, -16]
             })
         ,
         myPlacemark1 = new ymaps.Placemark([58.139312, 52.666964], {
@@ -26,7 +36,7 @@ ymaps.ready(function () {
                 iconLayout: 'default#image',
                 iconImageHref: '/images/cafe.png',
                 iconImageSize: [32, 32],
-                iconImageOffset: [0, 0]
+                iconImageOffset: [-16, -16]
             })
         ,
         myPlacemark2 = new ymaps.Placemark([58.139903, 52.680059], {
@@ -36,7 +46,7 @@ ymaps.ready(function () {
                 iconLayout: 'default#image',
                 iconImageHref: '/images/cafe.png',
                 iconImageSize: [32, 32],
-                iconImageOffset: [0, 0]
+                iconImageOffset: [-16, -16]
             })
         ,
         myPlacemark3 = new ymaps.Placemark([58.140203, 52.677312], {
@@ -46,19 +56,20 @@ ymaps.ready(function () {
                 iconLayout: 'default#image',
                 iconImageHref: '/images/cafe.png',
                 iconImageSize: [32, 32],
-                iconImageOffset: [0, 0]
+                iconImageOffset: [-16, -16]
             })
         ,
         myPlacemark4 = new ymaps.Placemark([58.139604, 52.675449], {
-            hintContent: 'Father's house',
-balloonContent: ''
-}, {
+            hintContent: 'Fathers house',
+            balloonContent: ''
+        }, {
                 iconLayout: 'default#image',
                 iconImageHref: '/images/cafe.png',
                 iconImageSize: [32, 32],
-                iconImageOffset: [0, 0]
+                iconImageOffset: [-16, -16]
             })
         ; myMap.geoObjects.add(myPlacemark0);
+    myMap.geoObjects.add(myPlacemark100);
     myMap.geoObjects.add(myPlacemark1);
     myMap.geoObjects.add(myPlacemark2);
     myMap.geoObjects.add(myPlacemark3);
